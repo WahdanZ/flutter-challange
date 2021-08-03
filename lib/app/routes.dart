3f4a8cbc7abba04ed7f86fa3ac62ae386abe737d
Ahmed Wahdan <ahmed.wahdan@outlook.com>
2021-08-03 08:31:08 +0200

    add route

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quandoo/app/route_observer.dart';
+import 'package:quandoo/features/merchant/presentation/pages/merchant_details_page.dart';
+import 'package:quandoo/features/merchant/presentation/pages/merchants_page.dart';
 import 'package:routemaster/routemaster.dart';
 
 final routeMaster = RoutemasterDelegate(
@@ -7,5 +9,13 @@ final routeMaster = RoutemasterDelegate(
   routesBuilder: (context) => routes,
 );
 final routes = RouteMap(routes: {
-  '/': (_) => MaterialPage(child: Container()),
+  '/': (_) => const MaterialPage(child: MerchantsPage()),
+  '${Routes.merchantDetailsRoute}/:id': (info) => MaterialPage(
+          child: MerchantDetailsPage(
+        id: info.pathParameters['id'],
+      )),
 });
+
+class Routes {
+  static const merchantDetailsRoute = '/merchant';
+}
